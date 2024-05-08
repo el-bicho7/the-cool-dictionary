dea7f028df3b6d2a3b6077931dc9244b6ea35bfe
Cristian Loera <loera_73@hotmail.com>
2024-05-08 14:20:30 -0600

    Added examples function and another api

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,46 @@ const result = $('#result');
 const modal = document.getElementById("myModal");
 const closeButton = document.getElementsByClassName("close")[0];
 
-// This function will fetch the dictionary and get the meaning of the word
-function meaningDict(){
+// this functionwill fetch theWord from stands4 and will return definition and example
+function exampleTest(){
+  const user_id = '12544';
+  const token = 'eu3x0aXkEo6Upvjl'
   const theWord = $('#inp-word').val().trim();
-  const url="https://api.dictionaryapi.dev/api/v2/entries/en/";
+  const format = 'json';
+  const url = `https://www.stands4.com/services/v2/syno.php?uid=${user_id}&tokenid=${token}&word=${theWord}&format=${format}`;
   
-  fetch(`${url}${theWord}`)
-    .then(response => response.json())
-    .then(data =>{
-      const meaning = $("<p>").append(data[0].meanings[0].definitions[0].definition);
-      $("#modal-meaning").append(meaning);
-    })
-    .catch(error => {
-      console.error("There was a problem with the Meaning operation", error)
-    })};
+  
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    data.result.forEach(dat => {
+      const terms = dat.term.split(", ");
+      
+      if (terms.includes(theWord)){
+        if (dat.definition !== "" ){
+          //This const will get the definition of theWord
+          const definition = $('<p>').append(dat.definition);
+          $('#modal-meaning').append(definition);
+        } else {
+          const definition = $('<p>').append(`Couldn't find a definition for ${theWord}`);
+          $('#modal-meaning').append(definition);
+          
+        };
+        if (dat.example.length > 0){
+          //This const will get the example
+          const example = $('<p>').append(dat.example);
+          $('#modal-example').append(example);
+        } else {
+          const example = $('<p>').append(`Couldn't find an exampe for ${theWord}`);
+          $('#modal-example').append(example);
+        } 
+    }}
+  )})
+  .catch(error =>{
+    console.error("There was a problem with the Dictionary operation", error);
+    $('#modal-example').append("Error in directory try another word");
+  })
+  };
 
 
 // This function gets the word from the Webster Dictionary and returns the word in spanish
@@ -33,19 +59,15 @@ function websterDictionary(){
       for (let dat of data){
         // data.hwi.hw = word stored
         if (theWord === dat.hwi.hw){
-          
           // This gets the word and the part of speech
           const partOfSent = $("<p>").append(dat.hwi.hw + " - " + dat.fl);
           $('#modal-word').append(partOfSent);
           // This gets the word in spanish
           const translate = $("<p>").append(dat.shortdef.join("\n"));
           $('#modal-translation').append(translate);
-          // This gets the example of the theWord
-          // const example = $("<p>").append(dat.def.sseq[0][0][1].dt[0][1].t);
-          // $('#modal-example').append(example);
   }}})
     .catch(error => {
-      console.error("There was a problem with the Dictionary operation", error)
+      console.error("There was a problem with the Dictionary operation", error);
     })};
 
   // This function gets the synonym and antonyms of the searched word
@@ -61,8 +83,13 @@ function thesaurusW(){
     for (let response of responses){
       if (theWord === response.hwi.hw){
         // This adds the synonyms
+        if (response.meta.syns.length > 0){
         const syn = $("<p>").append(response.meta.syns[0].join(", "));
         $('#modal-synonyms').append(syn);
+      } else {
+        const syn = $("<p>").append(`We couldn't find synonyms for ${theWord}`);
+          $('#modal-antonyms').append(syn);
+      };
         // This adds the antonyms
         if (response.meta.ants.length > 0){
           const ant = $("<p>").append(response.meta.ants[0].join(", "));
@@ -73,7 +100,9 @@ function thesaurusW(){
         }
       }}})
   .catch(error => {
-    console.error("There was a problem with the Thesaurus operation", error)
+    console.error("There was a problem with the Thesaurus operation", error);
+    $('#modal-synonyms').append("<p>Can't find synonyms, try another word.</p>");
+    $('#modal-antonyms').append("<p>Can't find antonyms, try another word.</p>");
   })};
 
 
@@ -84,15 +113,15 @@ function clearModal(){
   $('#modal-example').empty();
   $('#modal-synonyms').empty();
   $('#modal-antonyms').empty();
-  $('#modal-transalation').empty()
+  $('#modal-translation').empty()
 }
 
 // Webster Dictionary Spanish-English dictionary
 acceptBtn.on('click', function(){
   modal.style.display = 'block';
+  exampleTest();
   websterDictionary();
   thesaurusW();
-  meaningDict();
 });
 
 closeButton.addEventListener("click", function() {
